c766bda6d9b2fddf4bfbe6e205dcb2e8eed62e3e
shivam sharma <shivamsharma38391@gmail.com>
2025-09-30 22:34:56 +0530

    new ui code and blog post added

---
+++ magic-portfolio/src/components/ThemeToggle.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Row, ToggleButton, useTheme } from "@once-ui-system/core";
+
+export const ThemeToggle: React.FC = () => {
+
+
+  const systemTheme= () => {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    console.log("prefersDark", prefersDark);
+    return prefersDark ? 'dark' : 'light';
+  }
+
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const [currentTheme, setCurrentTheme] = useState("dark");
+
+  useEffect(() => {
+    setMounted(true);
+    // setCurrentTheme(document.documentElement.getAttribute("data-theme") || "light");
+
+    setCurrentTheme(systemTheme())
+  }, []);
+
+  useEffect(() => {
+    setCurrentTheme(document.documentElement.getAttribute("data-theme") || "light");
+  }, [theme]);
+
+  const icon = currentTheme === "dark" ? "light" : "dark";
+  const nextTheme = currentTheme === "light" ? "dark" : "light";
+
+  return (
+    <ToggleButton
+      prefixIcon={icon}
+      onClick={() => setTheme(nextTheme)}
+      aria-label={`Switch to ${nextTheme} mode`}
+    />
+  );
+};
---
+++ magic-portfolio/src/resources/content.tsx
@@ -0,0 +1,394 @@
+import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/types";
+import { Line, Logo, Row, Text } from "@once-ui-system/core";
+
+// const person: Person = {
+//   firstName: "Selene",
+//   lastName: "Yu",
+//   name: `Selene Yu`,
+//   role: "Design Engineer",
+//   avatar: "/images/avatar.jpg",
+//   email: "example@gmail.com",
+//   location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+//   languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+// };
+
+
+const person = {
+  firstName: "Shivam",
+  lastName: "Sharma",
+  get name() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  role: "Full stack developer",
+  avatar: "/images/avatar.jpg",
+
+  location: "Asia/Calcutta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  email: "shivamsharma38391@gmail.com",
+
+  timezone: "GMT+7", // Expecting the GMT offset, e.g., 'GMT+1' 
+  languages: ["English", "Hindi"], // optional: Leave the array empty if you don't want to display languages
+};
+
+const newsletter: Newsletter = {
+  display: true,
+  title: <>Subscribe to {person.firstName}'s Newsletter</>,
+  description: <>My weekly newsletter about creativity and engineering</>,
+};
+
+const social: Social = [
+  // Links are automatically displayed.
+  // Import new icons in /once-ui/icons.ts
+  {
+    name: "GitHub",
+    icon: "github",
+    link: "https://github.com/Shivam38391",
+  },
+  {
+    name: "LinkedIn",
+    icon: "linkedin",
+    link: "https://www.linkedin.com/in/shivam-sharma-6499061a9/",
+  },
+  {
+    name: "X",
+    icon: "x",
+    link: "",
+  },
+
+  {
+    name: "Threads",
+    icon: "threads",
+    link: "https://www.threads.net/@shivamsharma28391",
+  },
+  {
+    name: "Email",
+    icon: "email",
+    link: `mailto:${person.email}`,
+  },
+];
+
+const home: Home = {
+  path: "/",
+  image: "/images/og/home.jpg",
+  label: "Home",
+  title: `${person.name}'s Portfolio`,
+  description: `Portfolio website showcasing my work as a ${person.role}`,
+  headline: <>Full stack developer and sometime an artist</>,
+  featured: {
+    display: true,
+    title: (
+      <Row gap="12" vertical="center">
+        <strong className="ml-4">Once UI</strong>{" "}
+        <Line background="brand-alpha-strong" vert height="20" />
+        <Text marginRight="4" onBackground="brand-medium">
+          Featured work
+        </Text>
+      </Row>
+    ),
+    href: "/work/building-once-ui-a-customizable-design-system",
+  },
+  subline: (
+    <>
+            I'm Shivam sharma, a Fullstack developer , with over 3 years of experience.
+{" "}
+      {/* <Logo
+        dark
+        icon="/trademarks/wordmark-dark.svg"
+        style={{ display: "inline-flex", top: "0.25em", marginLeft: "-0.25em" }}
+      />
+      , where I craft intuitive
+      <br /> user experiences. After hours, I build my own projects. */}
+    </>
+  ),
+};
+
+const about: About = {
+  path: "/about",
+  label: "About",
+  title: `About – ${person.name}`,
+  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+  tableOfContent: {
+    display: true,
+    subItems: false,
+  },
+  avatar: {
+    display: true,
+  },
+  calendar: {
+    display: true,
+    link: "https://cal.com",
+  },
+  intro: {
+    display: true,
+    title: "Introduction",
+    description: (
+      <>
+      Full-Stack Developer with over 3 years of experience in building scalable web applications. Proficient in
+ frontend development using React.js and Next.js, with backend development experience through personal projects
+ using Django and Django REST Framework (DRF). Adept at collaborating in dynamic environments and passionate
+ about learning new technologies to enhance application performance and user experience.
+      </>
+    ),
+  },
+  work: {
+    display: true, // set to false to hide this section
+    title: "Work Experience",
+    experiences: [
+      {
+        company: "People central infotech Pvt. Ltd.",
+        timeframe: "2024 - Present",
+        role: "Frontend Developer",
+
+        achievements: [
+          <>
+             Optimized web applications for maximum speed and scalability, improving performance by 30%.
+          </>,
+          <>
+          Integrated applications with third-party software, including QuickBooks, various financial institutions, and accounting platforms like Xero.
+          </>,
+
+          <>
+            • Collaborated with designers and backend developers to enhance UI/UX and application functionality
+          </>
+        ],
+
+        images: [
+          // optional: leave the array empty if you don't want to display images
+          {
+            src: "/images/projects/project-01/cover-01.jpg",
+            alt: "Once UI Project",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+      {
+        company: "Auctopus Technology",
+        timeframe: "june2023 - feb-2023 ",
+        role: "Auctopus Technology",
+        achievements: [
+          <>
+            Assisted in backend development tasks using Django framework.
+          </>,
+          <>
+            • Gained hands-on experience in API development and database management.
+            • Collaborated with the development team on project planning and execution.
+
+          </>,
+        ],
+        images: [],
+      },
+    ],
+  },
+  studies: {
+    display: true, // set to false to hide this section
+    title: "Studies",
+    institutions: [
+      {
+        name: "Cluster University of jammu",
+        description: <>Bachelor of Science (B.Sc)</>,
+      },
+      // {
+      //   name: "Build the Future",
+      //   description: <>Studied online marketing and personal branding.</>,
+      // },
+    ],
+  },
+  technical: {
+    display: true, // set to false to hide this section
+    title: "Technical skills",
+    skills: [
+      {
+        title: "Django and Django REST framework",
+        description: <> For Developing backend apis </>,
+        // optional: leave the array empty if you don't want to display images
+        images: [
+          {
+            src: "/images/projects/project-01/django-cover.jpg",
+            alt: "django-cover",
+            width: 16,
+            height: 9,
+          },
+          {
+            src: "/images/projects/project-01/django-rest-framework.png",
+            alt: "Django REST framework",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+      {
+        title: "Next.js",
+        description: <> fullstack framework, for  Building next gen apps with Next.js + ShadcnUI</>,
+        // optional: leave the array empty if you don't want to display images
+        images: [
+          {
+            src: "/images/projects/project-01/Nextjs-recact.png",
+            alt: "Nextjs-recact.png",
+            width: 16,
+            height: 9,
+          },
+
+          {
+            src: "/images/projects/project-01/ShadcnUI.png",
+            alt: "ShadcnUI.png",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const blog: Blog = {
+  path: "/blog",
+  label: "Blog",
+  title: "Writing about design and tech...",
+  description: `Read what ${person.name} has been up to recently`,
+  // Create new blog posts by adding a new .mdx file to app/blog/posts
+  // All posts will be listed on the /blog route
+};
+
+const work: Work = {
+  path: "/work",
+  label: "Work",
+  title: `Projects – ${person.name}`,
+  description: `Design and dev projects by ${person.name}`,
+  // Create new project pages by adding a new .mdx file to app/blog/posts
+  // All projects will be listed on the /home and /work routes
+};
+
+// const gallery: Gallery = {
+//   path: "/gallery",
+//   label: "Gallery",
+//   title: `Photo gallery – ${person.name}`,
+//   description: `A photo collection by ${person.name}`,
+//   // Images by https://lorant.one
+//   // These are placeholder images, replace with your own
+//   images: [
+//     {
+//       src: "/images/gallery/horizontal-1.jpg",
+//       alt: "image",
+//       orientation: "horizontal",
+//     },
+//     {
+//       src: "/images/gallery/vertical-4.jpg",
+//       alt: "image",
+//       orientation: "vertical",
+//     },
+//     {
+//       src: "/images/gallery/horizontal-3.jpg",
+//       alt: "image",
+//       orientation: "horizontal",
+//     },
+//     {
+//       src: "/images/gallery/vertical-1.jpg",
+//       alt: "image",
+//       orientation: "vertical",
+//     },
+//     {
+//       src: "/images/gallery/vertical-2.jpg",
+//       alt: "image",
+//       orientation: "vertical",
+//     },
+//     {
+//       src: "/images/gallery/horizontal-2.jpg",
+//       alt: "image",
+//       orientation: "horizontal",
+//     },
+//     {
+//       src: "/images/gallery/horizontal-4.jpg",
+//       alt: "image",
+//       orientation: "horizontal",
+//     },
+//     {
+//       src: "/images/gallery/vertical-3.jpg",
+//       alt: "image",
+//       orientation: "vertical",
+//     },
+//   ],
+// };
+
+
+
+const gallery = {
+   path: "/gallery",
+  label: "Gallery",
+  title: "My photo gallery",
+  description: `A photo collection by ${person.name}`,
+  // Images from https://pexels.com
+  images: [
+    {
+      src: "/images/gallery/img-01.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-02.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-03.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-04.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-05.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-06.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-07.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-08.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-09.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-10.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-11.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-12.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-13.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-14.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+  ],
+};
+
+export { person, social, newsletter, home, about, blog, work, gallery };
