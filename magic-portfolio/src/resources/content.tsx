d43c56d49428fd6e16a9bef237a80cddc4f99760
shivam sharma <shivamsharma38391@gmail.com>
2026-07-07 20:00:39 +0530

    Update content.tsx

--- magic-portfolio/src/resources/content.tsx
+++ magic-portfolio/src/resources/content.tsx
@@ -118,7 +118,7 @@ const about: About = {
   },
   calendar: {
     display: true,
-    link: "https://cal.com",
+    link: "https://calendly.com/shivamsharma38391",
   },
   intro: {
     display: true,
